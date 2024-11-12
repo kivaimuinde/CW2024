c72890d667bcea9bed74909bc040023467ecf118
kivaimuinde <muindejosphat@gmail.com>
2024-11-12 12:30:08 +0300

    refactored code

--- src/main/java/com/example/demo/Projectile.java
+++ src/main/java/com/example/demo/Projectile.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 /**
  * The Projectile class represents a projectile (such as a bullet or fireball) that can be fired by a fighter plane or enemy.
- * It extends the ActiveActorDestructible class, meaning it is an active object that can be destroyed when it takes damage.
+ * It extends ActiveActorDestructible, indicating it is an active object that can be destroyed when it takes damage.
  */
 public abstract class Projectile extends ActiveActorDestructible {
 
@@ -11,25 +11,23 @@ public abstract class Projectile extends ActiveActorDestructible {
 	 *
 	 * @param imageName the name of the image file representing the projectile
 	 * @param imageHeight the height of the projectile image
-	 * @param initialXPos the initial x position of the projectile
-	 * @param initialYPos the initial y position of the projectile
+	 * @param initialXPos the initial x-position of the projectile
+	 * @param initialYPos the initial y-position of the projectile
 	 */
 	public Projectile(String imageName, int imageHeight, double initialXPos, double initialYPos) {
 		super(imageName, imageHeight, initialXPos, initialYPos);
 	}
 
 	/**
-	 * When the projectile takes damage, it is destroyed.
+	 * Destroys the projectile when it takes damage.
 	 */
 	@Override
 	public void takeDamage() {
-		this.destroy();
+		destroy();
 	}
 
 	/**
 	 * Updates the position of the projectile. This method must be implemented by subclasses to define the specific movement behavior.
-	 *
-	 * @see #updatePosition()
 	 */
 	@Override
 	public abstract void updatePosition();
